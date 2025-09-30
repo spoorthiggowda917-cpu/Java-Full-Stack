8e2064b784742b450ca6db4cea75e6fbfdb4d1c3
spoorthiggowda917-cpu <spoorthiggowda917@gmail.com>
2025-09-30 18:40:09 +0530

    Add files via upload

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,8 +1,9 @@
 package package1.basic;
 
 public class FirstProgram {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Sample s = new Sample();
+        s.display();
+    }
 }
---
+++ Sample.java
@@ -0,0 +1,10 @@
+package package1.basic;
+
+public class Sample {
+    int a;
+    int b;
+
+    public void display() {
+        System.out.println("Value of a and b is " + a + " " + b);
+    }
+}
