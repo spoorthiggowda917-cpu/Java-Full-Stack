1c1bd9af844150643abb8091e3ab65758c41532d
spoorthiggowda917-cpu <spoorthiggowda917@gmail.com>
2025-11-11 17:06:34 +0530

    Add files via upload

---
+++ StackDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.collection;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+		Stack s=new Stack();
+		s.push(200);
+		s.push(100);
+		System.out.println(s);
+		s.pop();
+		System.out.println(s);
+
+	}
+
+}
